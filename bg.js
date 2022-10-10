882746abe175ffe4207fd765ff5d26629de1352b
OScqlm <80879801+OScqlm@users.noreply.github.com>
2022-10-11 09:51:43 +1100

    Changing for .pcap or .PCAP files only

--- bg.js
+++ bg.js
@@ -10,18 +10,22 @@
 // found in the LICENSE file.
 
 chrome.downloads.onDeterminingFilename.addListener(function(item, __suggest) {
-    function suggest(filename, conflictAction) {
-      __suggest({filename: filename,
-                 conflictAction: conflictAction,
-                 conflict_action: conflictAction});
-      // conflict_action was renamed to conflictAction in
-      // https://chromium.googlesource.com/chromium/src/+/f1d784d6938b8fe8e0d257e41b26341992c2552c
-      // which was first picked up in branch 1580.
-    }
-    // create variable d and store the current date.
-    let d = new Date();
-    // store in  variable, the day, month and year. E.g. 31-5-2022
-    let fileDT = d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
-    suggest(fileDT + '/' + item.filename, 'uniquify'); 
-    return;
-  });
+
+  function suggest(filename, conflictAction) {
+    if(filename.split(".").pop().toLowerCase() != 'pcap') return
+    __suggest({filename: filename,
+               conflictAction: conflictAction,
+               conflict_action: conflictAction});
+    // conflict_action was renamed to conflictAction in
+    // https://chromium.googlesource.com/chromium/src/+/f1d784d6938b8fe8e0d257e41b26341992c2552c
+    // which was first picked up in branch 1580.
+  }
+  // create variable d and store the current date.
+  let d = new Date();
+  // store in  variable, the day, month and year. E.g. 31-5-2022
+  let fileDT = d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
+  suggest(fileDT + '/' + item.filename, 'uniquify'); 
+  return;
+});
+
+
